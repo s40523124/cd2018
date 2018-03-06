5d5fe1a5fac1744b0a86e0899053345bc6e747de
40523124 <s40523124@github.com>
2018-03-06 09:15:09 +0800

    '40523124_ag_set'

---
+++ 40523124/test.py
@@ -0,0 +1,67 @@
+'''print('ander awesome')
+'''
+#a = type('s')
+#print(a)
+'''with open ('2a.txt') as a:
+    b = a.readlines()
+    if 
+    print(b)'''
+
+#test1
+'''a = int(input('數字一: '))
+b = int(input('數字二: '))
+O = input('運算方式:')
+if O == ('*'):
+    print(a*b)
+else:
+    print('fuck you bitch')'''
+
+#test2
+'''
+with open('2a.txt') as a:
+    b = a.readlines()
+    b = [y.replace('\t', ';')for w in b]
+    b = [h.replace('\n', '')for w in b]
+    print(b)
+for n in range(len(y)):
+    print('第' + str(n+1) + '組:' + str(b[n])
+'''
+#test3
+'''
+C = int(input(':'))
+if C == int : 
+    for A in range(C):
+        print('第', A ,'組')
+else :
+print(NG)
+'''
+#test4
+'''with open("2a.txt") as fh:
+    #逐行讀入
+    data = fh.readlines() 
+    #將跳行字符取代為空白
+    data= [b.replace('\n', '') for b in data]
+    print(data)
+#將資料填上組別編號
+for num in range(len(b)):  
+print('第' + str(num+1) + '組:' + str(b[num]))
+team_list = list() #建立空list
+for i in range(len(data)):  
+    team = data[i].split()  #先將每一排切出來
+    for g in range(0,len(team),3):  
+        b = team[g:g+3]   #每三位學員為一組
+        team_list.append(b) #加入先前建立的空list
+for num in range(len(team_list)):  #將資料填上組別編號
+    print('第' + str(num+1) + '組:' + str(team_list[num]))
+'''
+#test5
+a = ['12','22','32','42']
+#print(len(a))
+for i in range(len(a)):
+    z = a[i].split
+    x = [c.strip() for c in a]
+print(x)
+
+'''a = [b.replace(':' , '/')for b in a]
+#b = a.strip()
+print(a.split('/'))'''
