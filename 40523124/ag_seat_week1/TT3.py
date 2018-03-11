bd3639febac3c5c81ece2f4e131afecdecd9afcc
40523124 <s40523124@github.com>
2018-03-11 21:14:29 +0800

    'new_blog'

--- 40523124/ag_seat_week1/TT3.py
+++ 40523124/ag_seat_week1/TT3.py
@@ -1,8 +1,31 @@
-a = [5, 4, 3, 2, 1]
+'''a = [5, 4, 3, 2, 1]
 #print(len(a))
 print(a[0:2])
 print(a[0:4])
 
 print(a[1])
-'''for i in range(len(a)):
-    print(a[i:i+2])'''
+for i in range(len(a)):
+    print(a[i:i+2])
+'''
+d = {}
+for a in range(3):
+    k = input('帳號: ')
+    v = str(input('密碼: '))
+    d[k] = v
+Target = 2
+for i in d:
+    if d[i] == 'fuck':
+        print(i, ': ', d[i])
+        break
+    else:
+        print('no find')
+
+#test
+'''
+for a in range(5):
+    if a <= 2 :
+        print(a)
+    else:
+        print(a)
+        break
+'''
